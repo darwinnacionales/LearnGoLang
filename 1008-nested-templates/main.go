d8ef6bff4c57b17e27c51cb69597ddd1d30d428e
Darwin Nacionales <darwin_nacionales@yahoo.com>
2020-06-15 14:52:45 +0800

    Nested Templates

---
+++ 1008-nested-templates/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl1.gohtml", nil)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
